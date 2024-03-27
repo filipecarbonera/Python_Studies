2214a63d4ed2f8c2c4e5b939b05f08b0cd46cb87
root <root@DESKTOP-MRVB4J3>
2024-03-27 19:57:27 -0300

    Inclusão do exercício 1.

---
+++ algorithm_basics/algorithms.py
@@ -0,0 +1,13 @@
+# 2.2.1. Calcular a area de um retangulo.
+print('''
+-----------------------------------------------------
+ CALCULO DE AREA DE RETANGULO
+-----------------------------------------------------
+''')
+
+largura = float(input('Largura desejada:'))
+altura = float(input('Altura desejada:'))
+
+area = largura * altura
+
+print(f'\nA area do retangulo e {area}.\n')
